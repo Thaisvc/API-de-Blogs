544e51de889b8f114e2dce9e883301c71c6a29e5
thais viana <thaisviana081@outlook.com>
2022-10-26 09:38:10 -0300

    🛑 CONFIGURANDO APP ROUTER 🛑

--- src/app.js
+++ src/app.js
@@ -10,4 +10,10 @@ app.use(express.json());
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
+
+// -----------------------------------------------
+const routers = require('./routers/routers');
+
+app.use(routers);
+
 module.exports = app;
