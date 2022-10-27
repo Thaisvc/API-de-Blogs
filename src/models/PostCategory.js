6b8abc3e694b92b0698ae9a9b2ed50113ebbe4fe
thais viana <thaisviana081@outlook.com>
2022-10-27 09:57:49 -0300

    📌 COMENTANDO  📝

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -21,18 +21,18 @@ module.exports = (sequelize, DataTypes) => {
     );
 
     PostCategory.associate = (models) => {
-        models.BlogPost.belongsToMany(models.Category, {
-            as: 'categories',
-            through: PostCategory,
-            foreignKey: 'post_id',
-            otherKey: 'category_id',
+        models.BlogPost.belongsToMany(models.Category, {  // ✒ TABELA BlogPost SE ASSOCIA A models.Category
+            as: 'categories', // ✒  ALIAS as: 'categories', SE REFERE A TABELA Category QUE ESTA SENDO ASSOCIADA
+            through: PostCategory, // ✒ NOME DA TAMELA DE INTER INTERCESSÃO (ESSA MESMO )
+            foreignKey: 'category_id', // ✒ foreignKey: 'category_id' SE REFERE A CHAVE ESTRANGEIRA DA TABELA Category
+            otherKey: 'post_id', // otherKey: 'post_id' E A OUTRA CHAVE ESTRANGERA REFERENTE A BlogPost
         });
         
         models.Category.belongsToMany(models.BlogPost, {
             as: 'blogPosts',
             through: PostCategory,
-            foreignKey: 'category_id',
-            otherKey: 'post_id',
+            foreignKey: 'post_id',
+            otherKey: 'category_id',
         });
     };
 
--- src/service/post.services.js
+++ src/service/post.services.js
@@ -4,8 +4,8 @@ const searchAllPost = async () => {
     const allPost = await BlogPost.findAll(
         {
             include: [
-                { model: User, as: 'user', attributes: { exclude: ['password'] } },
-                { model: Category, as: 'categories', through: { attributes: [] } },
+                { model: User, as: 'user', attributes: { exclude: ['password'] } }, // User É MINHA MODEL DEFINIDA EM MODEL sequelize.define('User',...) as: user  É O ALIAS/APELIDO QUE DEFINIR  NA MODEL BlogPost EM SUA ASSOCIAÇAO COM A TABELA/MODEL User
+                { model: Category, as: 'categories', through: { attributes: [] } }, // through É USADO EM TABELAS DE ASSOCIAÇAO
             ],
         },
     );
